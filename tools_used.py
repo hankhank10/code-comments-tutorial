746a59ff241593db9414a7e86a0003b689013aef
hankhank10 <m.a.hankinson@gmail.com>
2021-07-06 09:08:07 +0100

    Update tools_used.py

--- tools_used.py
+++ tools_used.py
@@ -11,13 +11,14 @@ import basics import html, css, js
 from jquery import notMuch
 
 # UI
-import halfmoon_css         # https://www.gethalfmoon.com/
-import fontawesome_icons    # https://fontawesome.com/
-import lineicons            # https://lineicons.com/
-import highlight_js         # https://highlightjs.org/
-import simplemde            # https://simplemde.com/
-import livestamp_js         # https://mattbradley.github.io/livestampjs/
-import typewriter_js        # https://safi.me.uk/typewriterjs/
+import halfmoon_css             # https://www.gethalfmoon.com/
+import fontawesome_icons        # https://fontawesome.com/
+import lineicons                # https://lineicons.com/
+import highlight_js             # https://highlightjs.org/
+import simplemde                # https://simplemde.com/
+import livestamp_js             # https://mattbradley.github.io/livestampjs/
+import typewriter_js            # https://safi.me.uk/typewriterjs/
+import jquery_loading_overlay   # https://gasparesganga.com/labs/jquery-loading-overlay
 
 # Python libraries
 import flask
